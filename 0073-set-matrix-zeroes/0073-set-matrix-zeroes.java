80a21e5a5329d1fb20ae5c42e48676289987b1fc
lokesh2506 <163889288+lokesh2506@users.noreply.github.com>
2026-09-03 11:27:39 +0530

    Time: 4 ms (11.48%), Space: 47.6 MB (61.79%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+        for(int i=0;i< matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j]==0){
+                    set1.add(i);
+                    set2.add(j);
+                }
+            }
+        }
+
+        for(int i=0;i< matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(set1.contains(i) || set2.contains(j)){
+                    matrix[i][j] =0;
+                }
+            }
+        }
+    }
+}
