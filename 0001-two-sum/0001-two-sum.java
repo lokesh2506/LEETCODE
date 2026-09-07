6ab2a7bfdfa52357c4e2ebd7bd1d2e23ce613a1a
lokesh2506 <163889288+lokesh2506@users.noreply.github.com>
2026-09-08 00:20:16 +0530

    Time: 2 ms (99.36%), Space: 46.7 MB (93.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,25 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,LinkedList<Integer>> map= new HashMap<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
 
-        for(int i=0;i<nums.length;i++){
-            LinkedList<Integer> list = map.getOrDefault(nums[i],new LinkedList<>());
-
-            list.add(i);
-            map.put(nums[i],list);
-
-        }
-
-       
         for(int i=0;i<nums.length;i++){
             int balance = target - nums[i];
-            LinkedList<Integer> list = map.getOrDefault(balance,new LinkedList<>());
-
-            if((nums[i] == balance) && (list.size() >= 2)) return new int[]{list.get(0),list.get(1)};
-            if((nums[i] != balance) && (list.size() != 0)) return new int[]{i,list.get(0)};
-
+            if(map.containsKey(balance)){
+                return new int[]{map.get(balance),i};
+                
+            }else{
+                map.put(nums[i],i);
+            }
         }
 
-        return new int[]{};
+        return new int[]{-1,-1};
     }
 }
