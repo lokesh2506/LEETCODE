c5b6c55166b7102a7ac8d70ad9807f8081371ce5
lokesh2506 <163889288+lokesh2506@users.noreply.github.com>
2026-09-23 23:32:00 +0530

    Time: 2 ms (92.6%), Space: 72.1 MB (43.87%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,35 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-
-        // size of the array
         int size = nums.length;
+        int [] result = new int[size];
 
-        HashMap<Integer,Integer> map1 = new HashMap<>();
-        HashMap<Integer,Integer> map2 = new HashMap<>();
-
-        // default value;
-        map1.put(0,1);
-        map2.put(size-1,1);
-
-        // Assign a values to the map1 i-1
-        for(int i=1;i<size;i++){
-            int preVal = map1.get(i-1); //0 -> 1
-            int product = nums[i-1] *  map1.get(i-1); // 1 * 1;
-            map1.put(i,product);
-        }
-
-        // Assign a values to the map2 i+1
-        for(int i= size-2;i>=0;i--){
-            int preVal = map2.get(i+1); //3 -> 1
-            int product = nums[i+1] *  map2.get(i+1); // 1 * 4;
-            map2.put(i,product); // 2 - > 4
-        }
-
+        int prefix = 1;
         for(int i=0;i<size;i++){
-            nums[i] = map1.get(i) * map2.get(i);
+            result[i] = prefix;
+            prefix *= nums[i];
         }
 
-        return nums;
-    }
+        int suffix = 1;
+        for(int j=size -1;j>=0;j--){
+            result[j] *= suffix;
+            suffix *= nums[j];
+        }
 
+        return result;
+    }
 }
