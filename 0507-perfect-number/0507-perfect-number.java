db68a3d21b3493f20a73be363b3355b2765b0530
lokesh2506 <163889288+lokesh2506@users.noreply.github.com>
2026-09-25 20:56:25 +0530

    Time: 2091 ms (46.82%), Space: 41.9 MB (79.33%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum = 0;
+        int size = num;
+
+        for(int i=1;i<=size/2;i++){
+            if(num % i == 0){
+                sum += i;
+            }
+        }
+
+        return sum == num;
+    }
+}
